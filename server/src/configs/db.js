ec2222f51a1ed86c77da8f65b6e4bce9507a7a81
Ramkrishna1597 <97449969+Ramkrishna1597@users.noreply.github.com>
2022-07-28 23:54:33 +0530

    add

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -7,3 +7,7 @@ const connect = () => {
 };
 
 module.exports = connect;
+
+
+// "mongodb+srv://zeeshannasir:zeeshan@cluster0.iltt7.mongodb.net/ecommerce"
+//mongo connect url => mongodb://ramkrishna:ram1234@ecommerce-shard-00-00.ybzfv.mongodb.net:27017,ecommerce-shard-00-01.ybzfv.mongodb.net:27017,ecommerce-shard-00-02.ybzfv.mongodb.net:27017/?ssl=true&replicaSet=atlas-s2d4cr-shard-0&authSource=admin&retryWrites=true&w=majority
